72301fb7e73747f3e2d5a6017c220107b07646e6
Tncy <tuncay_sukan@hotmail.com>
2024-01-06 04:18:09 +0300

    jquery işlemler
    jquery kodları optimize edildi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,94 +3,100 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sedeus Api</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>Sticky Menü</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
-<style>
-    body {
-        margin: 0;
-        padding: 0;
-    }
-
-    .header {
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        background-color: #f1f1f1;
-        padding: 10px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        z-index: 7;
-    }
-
-    .logo img {
-        max-width: 100%;
-        height: auto;
-    }
-
-    .slider {
-        display: flex;
-        gap: 20px;
-        align-items: center;
-    }
-
-    .slider p {
-        margin: 0;
-        overflow: hidden; 
-        white-space: nowrap;
-    }
-
-    .slider .slide-animation {
-        animation: slide 5s linear infinite; 
-    }
-
-    .icon {
-        margin: 30px;
-    }
-
-    @keyframes slide {
-        0% {
-            transform: translateX(100%); 
-        }
-        100% {
-            transform: translateX(-100%); 
-        }
-    }
-
-</style>
 <body>
 
 <div id="veri-container"></div> 
 
 <script>
     $(document).ready(function () {
-        var categoryId = 4; 
-    var apiUrl = "http://127.0.0.1:8000/test/" + categoryId;
-    
+        var categoryId = 2; 
+        var apiUrl = "http://127.0.0.1:8000/test/" + categoryId;
+
         $.get(apiUrl, function (veriler) {
             console.log(veriler);
             $.each(veriler, function (index, veri) {
-                var bannerHtml = '<div class="header">';
-                bannerHtml += '<div class="logo">';
-                bannerHtml += '<img  width="60" height="60" src="{{url('')}}/'+veri.images + '" alt="Logo">';
-                bannerHtml += '</div>';
-                bannerHtml += '<div class="slider">';
-                bannerHtml += '<p class="slide-animation">' + veri.button_text + '</p>';
-                bannerHtml += '<p class="slide-animation">' + veri.svg_code + '</p>';
-                bannerHtml += '</div>';
-                bannerHtml += '<div class="icon">';
-                bannerHtml += '<i class="fa ' + veri.icon + '"></i>';
-                bannerHtml += '</div>';
-                bannerHtml += '</div>';
+                var header = $('<div class="header"></div>');
 
-                $("#veri-container").append(bannerHtml);
+                var logo = $('<div class="logo"></div>');
+                var logoImg = $('<img>').attr({
+                    'width': '60',
+                    'height': '60',
+                    'src': '{{  asset('storage') }}'+'/'+ veri.images ,
+                    'alt': 'Logo'
+                });
+                logo.append(logoImg);
+
+                var slider = $('<div class="slider"></div>');
+                var buttonText = $('<p></p>').text(veri.button_text);
+                var svgCode = $('<p></p>').text(veri.svg_code);
+                slider.append(buttonText, svgCode);
+
+                var icon = $('<div class="icon"></div>');
+                var iconElement = $('<i class="fa"></i>').addClass(veri.icon);
+                icon.append(iconElement);
+
+                header.append(logo, slider, icon);
+
+                $("body").append(header);
+
+                header.css({
+                    'position': 'fixed',
+                    'bottom': '0',
+                    'left': '0',
+                    'width': '100%',
+                    'background-color': '#f1f1f1',
+                    'padding': '0',
+                    'display': 'flex',
+                    'justify-content': 'space-between',
+                    'align-items': 'center',
+                    'z-index': '7',
+                    'height': '8%'
+                });
+
+                logo.find('img').css({
+                    'max-width': '100%',
+                    'height': 'auto'
+                });
+
+                slider.css({
+                    'display': 'flex',
+                    'gap': '20px',
+                    'align-items': 'center'
+                });
+
+                slider.find('p').css({
+                    'margin': '0',
+                    'overflow': 'hidden',
+                    'white-space': 'nowrap',
+                    'animation': 'slide 5s linear infinite'
+                });
             });
         });
     });
 </script>
+
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    @keyframes slide {
+        0% {
+            transform: translateX(100%);
+        }
+        100% {
+            transform: translateX(-100%);
+        }
+    }
+
+    .slide-animation {
+        animation: slide 5s linear infinite;
+    }
+</style>
 
 </body>
 </html>
